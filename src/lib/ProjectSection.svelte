815a779c4bc66e5747d59e02bf93ae1a0c551f6a
99toxic <nsimpson1983@gmail.com>
2021-12-10 10:52:00 -0500

    Created loop for project section on homepage and a store for project object

--- src/lib/ProjectSection.svelte
+++ src/lib/ProjectSection.svelte
@@ -1,135 +1,28 @@
 <script>
 	import Heading from './Heading.svelte';
+	import { projects } from '../stores/projectImages';
 	let heading = 'Our Projects';
 	let subHeading = 'Expertly designed and fully optimized websites.';
 </script>
 
 <div class="projects">
 	<Heading {heading} {subHeading} />
-	<div class="projects__wrap">
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/engergyn-website-design.jpg" alt="Energyn" />
+	<div class="wrap">
+		{#each projects as project}
+			<div class="item">
+				<div class="image">
+					<img src={project.image} alt={project.title} />
+				</div>
+				<!-- //# Will add this later (part of projectImages)-->
+				<!-- <div class="text">
+					<h3>{project.title}</h3>
+					<p>
+						{project.description}
+					</p>
+					<a href="/">Read More</a>
+				</div> -->
 			</div>
-			<div class="projects__item__text">
-				<h3>Energyn</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/jwrestores-website-design.jpg" alt="JW Restores" />
-			</div>
-			<div class="projects__item__text">
-				<h3>JW Restores</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/bluelineelectric-website-design.jpg" alt="Blue Line Electric" />
-			</div>
-			<div class="projects__item__text">
-				<h3>Blue Line Electric</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img
-					src="images/projects/jadespremiumqualitycleaning-website-design.jpg"
-					alt="Jades Premium quality Cleaning"
-				/>
-			</div>
-			<div class="projects__item__text">
-				<h3>Jades Premium quality Cleaning</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img
-					src="images/projects/ridgeappliancerepair-website-design.jpg"
-					alt="Ridge Appliance Repair"
-				/>
-			</div>
-			<div class="projects__item__text">
-				<h3>Ridge Appliance Repair</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/totalroofingofatl-website-design.jpg" alt="Total Roofing ATL" />
-			</div>
-			<div class="projects__item__text">
-				<h3>Total Roofing ATL</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/arhthomesolutions-website-design.jpg" alt="Blue Line Electric" />
-			</div>
-			<div class="projects__item__text">
-				<h3>Blue Line Electric</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/imperialchemdry-website-design.jpg" alt="Imperial Chem Dry" />
-			</div>
-			<div class="projects__item__text">
-				<h3>Imperial Chem dry</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
-		<div class="projects__item">
-			<div class="projects__item__image">
-				<img src="images/projects/hammondhill-website-design.jpg" alt="Hammond Hill" />
-			</div>
-			<div class="projects__item__text">
-				<h3>Hammond Hill</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, tortor eu
-					consectetur congue, nisi nisl aliquet nisl, eget consectetur nunc nisl euismod nunc.
-				</p>
-				<a href="/">Read More</a>
-			</div>
-		</div>
+		{/each}
 	</div>
 </div>
 
@@ -142,16 +35,16 @@
 		justify-content: space-around;
 		padding: 20rem;
 	}
-	.projects__wrap {
+	.wrap {
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
 		grid-gap: 4rem;
 		margin-top: 5rem;
 	}
-	.projects__item {
+	.item {
 		width: 100%;
 	}
-	.projects__item__image img {
+	.image img {
 		width: 100%;
 	}
 </style>
