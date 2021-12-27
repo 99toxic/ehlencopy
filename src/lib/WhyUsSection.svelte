fc97b247b8b609a84696ab359e6e6461b7ce894e
99toxic <nsimpson1983@gmail.com>
2021-12-27 16:56:36 -0500

    Worked on why-us section and cleaned up some stuff

--- src/lib/WhyUsSection.svelte
+++ src/lib/WhyUsSection.svelte
@@ -1,35 +1,49 @@
+<script>
+	import Heading from './Heading.svelte';
+	let heading = 'Why Us';
+	let subHeading = `Ehlen Analytics strives to bring each of our clients individualized, professional digital
+				marketing solutions. We use an unmatched level of attention to detail and care with each and
+				every project.`;
+</script>
+
 <div class="why-us">
 	<div class="row-1">
 		<div class="global-reach diamond">
-			<i class="fas fa-globe" />
-			<h3>Global Reach</h3>
+			<div class="rotate">
+				<i class="fas fa-globe" />
+				<h3>Global Reach</h3>
+			</div>
 		</div>
 		<div class="communication diamond">
-			<i class="fas fa-users" />
-			<h3>Communication</h3>
+			<div class="rotate">
+				<i class="fas fa-users" />
+				<h3>Communication</h3>
+			</div>
 		</div>
 	</div>
 	<div class="heading diamond">
-		<h2>Why Us?</h2>
-		<hr />
-		<p>
-			Ehlen Analytics strives to bring each of our clients individualized, professional digital
-			marketing solutions. We use an unmatched level of attention to detail and care with each and
-			every project.
-		</p>
+		<div class="rotate">
+			<Heading {heading} {subHeading} />
+		</div>
 	</div>
 	<div class="row-2">
 		<div class="experienced diamond">
-			<i class="fas fa-briefcase" />
-			<h3>Experienced</h3>
+			<div class="rotate">
+				<i class="fas fa-briefcase" />
+				<h3>Experienced</h3>
+			</div>
 		</div>
 		<div class="best-practices diamond">
-			<i class="fas fa-trophy" />
-			<h3>Best Practices</h3>
+			<div class="rotate">
+				<i class="fas fa-trophy" />
+				<h3>Best Practices</h3>
+			</div>
 		</div>
 		<div class="personalized diamond">
-			<i class="fas fa-magic" />
-			<h3>Personalized</h3>
+			<div class="rotate">
+				<i class="fas fa-magic" />
+				<h3>Personalized</h3>
+			</div>
 		</div>
 	</div>
 </div>
@@ -37,7 +51,11 @@
 <style>
 	.why-us {
 		background-color: var(--color-tertiary);
-		margin-top: 10rem;
+		margin-top: 20rem;
+	}
+
+	.rotate {
+		transform: rotate(-45deg);
 	}
 
 	.row-1 {
@@ -53,17 +71,18 @@
 	}
 
 	.diamond {
-		/* put items in middle */
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
-		align-items: center;
 		margin: 0 auto;
 		transform: rotate(45deg);
 	}
 
 	.heading {
-		background: var(--color-primary);
+		background: var(--color-white);
 		height: 40rem;
+		margin-bottom: 50px;
+		text-align: center;
 		width: 40rem;
 	}
 
@@ -72,7 +91,6 @@
 		height: 20rem;
 		margin: -5rem 20rem 0 20rem;
 		text-align: center;
-		transform: rotate(45deg);
 		width: 20rem;
 	}
 
@@ -81,47 +99,26 @@
 		height: 20rem;
 		margin: 5rem 20rem -5rem 20rem;
 		text-align: center;
-		transform: rotate(45deg);
 		width: 20rem;
-	}
-
-	.heading {
-		text-align: center;
-		margin: 0 auto;
-		margin-bottom: 50px;
-	}
-
-	h2 {
-		font-size: 2.5rem;
-		font-weight: 700;
-		margin-bottom: 20px;
 	}
 
 	h3 {
 		font-size: 1.5rem;
 		font-weight: 700;
 		margin-bottom: 20px;
+		color: var(--color-white);
 	}
 
 	hr {
 		width: 50px;
 		height: 2px;
 		background-color: #000;
-		margin: 0 auto;
-		margin-bottom: 30px;
-	}
-
-	i,
-	h2,
-	h3,
-	hr,
-	p {
-		transform: rotate(-45deg);
-		color: var(--color-white);
+		margin: 30px auto;
 	}
 
 	i {
 		font-size: 3rem;
 		margin-bottom: 20px;
+		color: var(--color-white);
 	}
 </style>
